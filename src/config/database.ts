cafe28b604fe0d3a9832dfaf81b394e9078ff709
Luis Edward Miranda <luisedward.miranda@gmail.com>
2023-10-08 00:53:06 +0800

    chore: change the name of the sqlite db

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,7 +2,7 @@ import { Sequelize } from 'sequelize';
 
 const sequelize = new Sequelize({
   dialect: 'sqlite',
-  storage: 'database.sqlite', // Path to your SQLite database file
+  storage: 'auth-service.sqlite', // Path to your SQLite database file
 });
 
 export default sequelize;
