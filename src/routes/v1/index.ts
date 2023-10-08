784fb2c4d4e389df4433f2f186d2ccca2fb45eb6
Luis Edward Miranda <luisedward.miranda@gmail.com>
2023-10-08 21:34:44 +0800

    chore: code cleaning

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,8 +1,3 @@
-// import { Router } from 'express';
-// import validate from '../../middleware/validate';
-// import { loginSchema, signupSchema } from '../../validations/auth.validation';
-// import * as authController from '../../controller/auth.controller';
-
 import Elysia from "elysia";
 import authRouter from "./auth.router";
 
--- src/routing.ts
+++ src/routing.ts
@@ -4,38 +4,21 @@
  * @typedef {import('elysia').Elysia} Elysia
  */
 import Elysia from "elysia";
-import authRouter from "./routes/v1/auth.router";
 import v1 from "./routes/v1";
 
 const app = new Elysia();
-app.get('/', () => 'Hello Elysia 123');
 
-
-// if (config.node_env === 'production') {
-// 	app.use('/api/v1/auth', authLimiter);
-// }
+app.get('/health', () => 'OK');
 
 app.use(v1);
 
-// app.use('/api/v1', passwordRouter);
+app.all("*", (request) => {
+	request.set.status = 404;
 
-// app.use('/api/v1', verifyEmailRouter);
-
-// app.get('/secret', isAuth, (_req, res) => {
-// 	res.json({
-// 		message: 'You can see me'
-// 	});
-// });
-
-// app.all('*', (req, res) => {
-// 	res.status(404);
-// 	if (req.accepts('html')) {
-// 		res.sendFile(path.join(__dirname, 'views', '404.html'));
-// 	} else if (req.accepts('json')) {
-// 		res.json({ error: '404 Not Found' });
-// 	} else {
-// 		res.type('txt').send('404 Not Found');
-// 	}
-// });
+	return {
+		"success": false,
+		"message": "404 Not Found"
+	}
+})
 
 export default app;
